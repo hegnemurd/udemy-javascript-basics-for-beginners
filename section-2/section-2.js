274a71316937a6d82f1f29d043d91dc0707e3a9f
hegnemurd <hegne.murd@gmail.com>
2022-07-24 14:52:08 +0100

    Section 2

---
+++ section-2/section-2.js
@@ -0,0 +1,103 @@
+// ----- VARIABLES -----
+
+// let name = "Potato";
+// console.log(name);
+
+// let interestRate = 0.3;
+// interestRate = 1;
+// console.log(interestRate);
+// let to be used for variables that might change
+
+// Cannot be a reserved keyword
+// Should be meaningful
+// Cannot start with a nuber (1name)
+// Cannot contain a space or hyphen (-)
+// Should use camelCase
+// Are case-sensitive
+// let firstName = "Potato";
+// let lastName = "Tomato";
+
+// ----- CONSTANTS -----
+
+// const name = "Carrot";
+// console.log(name);
+// const to be used for constants that will not change
+
+// ----- PRIMITIVE TYPES  -----
+
+// let name = "Potato"; // String Literal
+// let age = 20; // Number Literal
+// let isApproved = true; // Boolean Literal (true or false)
+// let firstName = undefined;
+// let selectedColour = null; // cleared value of the variable
+
+// ----- DYNAMIC TYPING -----
+
+// let name = "Potato"; // string type
+// let age = 20; // number type
+// let isApproved = true; // boolean type
+// let firstName = undefined; // undefined type
+// let selectedColour = null; // object type
+
+// ----- OBJECTS -----
+
+// let vegetable = {
+//   name: "Potato",
+//   age: 20,
+// };
+
+// Dot notation
+// vegetable.name = "Tomato";
+
+// Bracket notation
+// vegetable["name"] = "Carrot";
+// OR
+// let selection = "name";
+// vegetable[selection] = "Carrot";
+
+// console.log(vegetable.name, vegetable.age);
+
+// ----- ARRAYS -----
+
+// let selectedColours = ["purple", "blue", "yellow", "green"];
+// selectedColours[4] = "red";
+// selectedColours[4] = 10;
+// console.log(selectedColours[3]);
+// console.log(selectedColours);
+
+// ----- FUNCTIONS -----
+
+// SINGLE PARAMETER
+// function greet(name) {
+//   // name is a parameter to the function, a placeholder for the value - argument
+//   console.log("Hello " + name + "!");
+//   //   console.log("Hello world!");
+//   //   alert("Hello there!");
+// }
+
+// greet("Carrot"); // Carrot is an argument to the function, a value to the parameter
+
+// MULTIPLE PARAMETER
+// function greet(name, lastName) {
+//   console.log("Hello " + name + " " + lastName + "!");
+// }
+
+// greet("Carrot", "Sticks");
+
+// ----- TYPES OF FUNCTIONS -----
+
+// PERFORMING A TASK
+// function greet(name, lastName) {
+//   console.log("Hello " + name + " " + lastName + "!");
+// }
+
+// greet("Carrot", "Sticks");
+
+// CALCULATING A VALUE
+// function square(number) {
+//   return number * number;
+// }
+
+// console.log(square(2));
+// let number = square(2);
+// console.log(number);

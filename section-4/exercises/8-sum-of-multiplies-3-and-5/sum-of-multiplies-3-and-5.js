b56ac315c5cc351679db470aff5f7047a1a10183
hegnemurd <hegne.murd@gmail.com>
2022-07-25 22:01:03 +0100

    Section 4

--- section-4/exercises/8-sum-of-multiplies-3-and-5/sum-of-multiplies-3-and-5.js
+++
@@ -1,3 +0,0 @@
-// ** My solution **
-
-// ** Their solution **
--- section-4/exercises/9-grade/grade.js
+++ section-4/exercises/9-grade/grade.js
@@ -1,3 +1,51 @@
 // ** My solution **
 
+// Average = 70
+// 0-59: F
+// 60-69: D
+// 70-79: C
+// 80-89: B
+// 90-100: A
+
+const marks = [80, 80, 50];
+
+console.log(calculateGrade(marks));
+
+function calculateGrade(marks) {
+  let sum = 0;
+  for (let i in marks) {
+    sum += marks[i];
+  }
+  let average = sum / marks.length;
+  if (average >= 0 && average <= 59) {
+    return "F";
+  } else if (average >= 60 && average <= 69) {
+    return "D";
+  } else if (average >= 70 && average <= 79) {
+    return "C";
+  } else if (average >= 80 && average <= 89) {
+    return "B";
+  } else if (average >= 90 && average <= 100) {
+    return "A";
+  }
+}
+
 // ** Their solution **
+// const marks = [80, 80, 50];
+
+// console.log(calculateGrade(marks));
+
+// function calculateGrade(marks) {
+//   const average = calculateAverage(marks);
+//   if (average < 60) return "F";
+//   if (average < 70) return "D";
+//   if (average < 80) return "C";
+//   if (average < 90) return "B";
+//   return "A";
+// }
+
+// function calculateAverage(array) {
+//   let sum = 0;
+//   for (let value of array) sum += value;
+//   return sum / array.length;
+// }

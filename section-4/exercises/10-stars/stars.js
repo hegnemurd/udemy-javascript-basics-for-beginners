f94a9f3ec59cbcd7224bcf250ab6b6e6c7e72f63
hegnemurd <hegne.murd@gmail.com>
2022-07-25 22:12:13 +0100

    Section 4

--- section-4/exercises/10-stars/stars.js
+++ section-4/exercises/10-stars/stars.js
@@ -1,3 +1,24 @@
 // ** My solution **
 
+showStars(5);
+
+function showStars(rows) {
+  const star = "*";
+  let rowStars = "";
+  for (let i = 0; i < rows; i++) {
+    rowStars += star;
+    console.log(rowStars);
+  }
+}
+
 // ** Their solution **
+
+// showStars(5);
+
+// function showStars(rows) {
+//   for (let row = 1; row <= rows; row++) {
+//     let pattern = "";
+//     for (let i = 0; i < row; i++) pattern += "*";
+//     console.log(pattern);
+//   }
+// }

10d653a7b3f937ce8c7ba06366275ea78d1885d7
hegnemurd <hegne.murd@gmail.com>
2022-07-25 21:04:08 +0100

    Section 4

--- section-4/exercises/7-string-properties/string-properties.js
+++ section-4/exercises/7-string-properties/string-properties.js
@@ -1,3 +1,36 @@
 // ** My solution **
 
+const movie = {
+  title: "a",
+  releaseYear: 2018,
+  rating: 4.5,
+  director: "b",
+};
+
+showProperties(movie);
+
+function showProperties(obj) {
+  for (let key in movie) {
+    if (typeof movie[key] === "string") {
+      console.log(key, movie[key]);
+    }
+  }
+}
+
 // ** Their solution **
+
+// const movie = {
+//   title: "a",
+//   releaseYear: 2018,
+//   rating: 4.5,
+//   director: "b",
+// };
+
+// showProperties(movie);
+
+// function showProperties(obj) {
+//   for (let key in obj) {
+//     if (typeof obj[key] === "string")
+//       console.log(key, obj[key]);
+//   }
+// }

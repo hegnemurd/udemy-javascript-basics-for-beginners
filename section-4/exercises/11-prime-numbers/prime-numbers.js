2de570f65a9fbd58999474feec3a4453f7821ccc
hegnemurd <hegne.murd@gmail.com>
2022-07-26 21:28:31 +0100

    Section 4

--- section-4/exercises/11-prime-numbers/prime-numbers.js
+++ section-4/exercises/11-prime-numbers/prime-numbers.js
@@ -1,3 +1,30 @@
 // ** My solution **
 
+// Prime (whose factors are only 1 and itself) ie 11, 13 etc
+// Composite ie 12, 14 etc
+
+// loop through 20, 1-20
+// if statement i/1 && i/i
+// if statement = true
+// loop through all numbers up until i; 1-i
+// if comes back false then console.log("i");
+// if comes back true, then nothing is returned
+// then continue to the next number
+
+showPrimes(20);
+
+function showPrimes(limit) {
+  for (let i = 0; i <= limit; i++) {
+    if (i / 1 && i / i) {
+      for (let index = 2; index <= i; index++) {
+        if (i / index !== 0) {
+          console.log(i);
+        } else {
+          continue;
+        }
+      }
+    }
+  }
+}
+
 // ** Their solution **

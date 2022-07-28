ac3e2027a063f7aaa616206d9be84612efb820d0
hegnemurd <hegne.murd@gmail.com>
2022-07-28 21:20:48 +0100

    Section 5 - Factory and Constructor Functions

--- section-5/part-one.js
+++ section-5/part-one.js
@@ -1,18 +1,50 @@
 // ----- BASICS -----
 
 // Object-Oriented Programming (OOP)
-const circle = {
-  radius: 1,
-  location: {
-    x: 1,
-    y: 1,
-  },
-  isVisible: true,
-  draw: function () {
-    console.log("draw");
-  },
-};
+// const circle = {
+//   radius: 1,
+//   location: {
+//     x: 1,
+//     y: 1,
+//   },
+//   isVisible: true,
+//   draw: function () {
+//     console.log("draw");
+//   },
+// };
 
-circle.draw(); // Method
+// circle.draw(); // Method
 
 // ----- FACTORY FUNCTIONS -----
+
+// function createCircle(radius) {
+//   return {
+//     radius,
+//     draw() {
+//       console.log("draw");
+//     },
+//   };
+// }
+
+// const circle1 = createCircle(1);
+// console.log(circle1);
+
+// const circle2 = createCircle(2);
+// console.log(circle2);
+
+// ----- CONSTRUCTOR FUNCTIONS -----
+
+// use Pascal notation: OneTwoThreeFour
+// function Circle(radius) {
+//   this.radius = radius;
+//   this.draw = function () {
+//     console.log("draw");
+//   };
+//   // return this
+// }
+
+// const circle = new Circle(1);
+// // const x = {}; 
+
+// ----- DYNAMIC NATURE OF OBJECTS -----
+

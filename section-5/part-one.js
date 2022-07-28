a0534c4a4112899d012c5443eff961a9e91c6641
hegnemurd <hegne.murd@gmail.com>
2022-07-28 20:58:57 +0100

    Section 5 - Basics

---
+++ section-5/part-one.js
@@ -0,0 +1,18 @@
+// ----- BASICS -----
+
+// Object-Oriented Programming (OOP)
+const circle = {
+  radius: 1,
+  location: {
+    x: 1,
+    y: 1,
+  },
+  isVisible: true,
+  draw: function () {
+    console.log("draw");
+  },
+};
+
+circle.draw(); // Method
+
+// ----- FACTORY FUNCTIONS -----

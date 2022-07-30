8f776e3b79320cfa41e330cd32e0ae39b1d0aeb2
hegnemurd <hegne.murd@gmail.com>
2022-07-30 18:50:39 +0100

    Section 5 - Exercise 4

---
+++ section-5/exercises/4-blog-post-object/blog-post-object.js
@@ -0,0 +1,38 @@
+// ----- MY SOLUTION -----
+
+// title
+// body
+// author
+// views
+// comments (author, body)
+// isLive (true or false)
+
+let blogPost = {
+  title: "Title",
+  body: "...Some text for the body...",
+  author: "Author",
+  views: 12,
+  comments: [
+    { author: "Comment Author", body: "...Comment..." },
+    { author: "Another Comment Author", body: "...Comment Body..." },
+  ],
+  islive: true,
+};
+
+console.log(blogPost);
+
+// ----- THEIR SOLUTION -----
+
+let post = {
+  title: "a",
+  body: "b",
+  author: "C",
+  views: 10,
+  comments: [
+    { author: "a", body: "b" },
+    { author: "c", body: "d" },
+  ],
+  islive: true,
+};
+
+console.log(post);

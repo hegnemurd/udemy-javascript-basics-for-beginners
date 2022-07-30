e7e74b76f5daf11cfdeca71bffc65c254159947c
hegnemurd <hegne.murd@gmail.com>
2022-07-30 15:43:53 +0100

    Section 5 - Exercise 1

---
+++ section-5/exercises/1-address-object/address-object.js
@@ -0,0 +1,36 @@
+// ----- MY SOLUTION -----
+// street
+// city
+// zipcode
+// func showAddress(address) - display all properties with value
+
+// const address = {
+//   street: "Street",
+//   city: "City",
+//   zipCode: "ZipCode",
+// };
+
+// function showAddress(address) {
+//   for (let key in address) {
+//     console.log(key, address[key]);
+//   }
+// }
+
+// showAddress(address);
+
+// ----- THEIR SOLUTION -----
+
+const address = {
+    street: "a",
+    city: "b",
+    zipCode: "c",
+  };
+  
+  function showAddress(address) {
+    for (let key in address) {
+      console.log(key, address[key]);
+    }
+  }
+  
+  showAddress(address);
+  

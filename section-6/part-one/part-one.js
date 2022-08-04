1293a51504707ecb33749ce0c723cb2b7e45ad43
hegnemurd <hegne.murd@gmail.com>
2022-08-04 21:45:38 +0100

    Section 6 - Arrow Functions

--- section-6/part-one/part-one.js
+++ section-6/part-one/part-one.js
@@ -50,3 +50,14 @@
 //   });
 
 // console.log(course);
+
+// ----- ARROW FUNCTIONS -----
+
+const courses = [
+  { id: 1, name: "a" },
+  { id: 2, name: "b" },
+];
+
+const course = courses.find((course) => course.name === "b");
+
+console.log(course);

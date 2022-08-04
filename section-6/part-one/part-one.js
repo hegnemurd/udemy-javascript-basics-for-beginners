0c01c0f732d23761473543bc99b934ccc235beb2
hegnemurd <hegne.murd@gmail.com>
2022-08-04 21:39:01 +0100

    Section 6 - Finding Elements (Reference Types)

--- section-6/part-one/part-one.js
+++ section-6/part-one/part-one.js
@@ -16,18 +16,37 @@
 
 // ----- FINDING ELEMENTS (PRIMITIVES) -----
 
-const numbers = [1, 2, 3, 1, 4];
+// const numbers = [1, 2, 3, 1, 4];
 
 // shows -1, since it is not in the array
-console.log(numbers.indexOf("a"));
+// console.log(numbers.indexOf("a"));
 // shows 0, since it is in that position
-console.log(numbers.indexOf(1));
+// console.log(numbers.indexOf(1));
 // shows 3, since the second argument states where to start counting from and it counts only the second number 1
-console.log(numbers.indexOf(1, 2));
+// console.log(numbers.indexOf(1, 2));
 // shows 3, since the last 1 in the array is in that position
-console.log(numbers.lastIndexOf(1));
+// console.log(numbers.lastIndexOf(1));
 
 // if this return true, then the value exists in the array
-console.log(numbers.indexOf(1) !== -1);
+// console.log(numbers.indexOf(1) !== -1);
 // nicer way of writing the same as above
-console.log(numbers.includes(1));
+// console.log(numbers.includes(1));
+
+// ----- FINDING ELEMENTS (OBJECTS) -----
+
+// const courses = [
+//   { id: 1, name: "a" },
+//   { id: 2, name: "b" },
+// ];
+
+// Returns the first course object that satisfies the criteria for this array
+// const course = courses.find(function (course) {
+//   return course.name === "b";
+// });
+
+// Return the first course object's index, which satifies the criteria for this array
+// const course = courses.findIndex(function (course) {
+//     return course.name === "b";
+//   });
+
+// console.log(course);

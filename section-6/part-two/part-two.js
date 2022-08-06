ea2b6735b92cfc0f93c77037ce1f16ec388a50ba
hegnemurd <hegne.murd@gmail.com>
2022-08-06 21:33:48 +0100

    Section 6 - Sorting Arrays

--- section-6/part-two/part-two.js
+++ section-6/part-two/part-two.js
@@ -22,3 +22,33 @@
 
 // const combined = parts.join("-");
 // console.log(combined);
+
+// ----- SORTING ARRAYS -----
+
+// const numbers = [2, 3, 1];
+// numbers.sort();
+// console.log(numbers);
+
+// numbers.reverse();
+// console.log(numbers);
+
+// const courses = [
+//   { id: 1, name: "Node.js" },
+//   { id: 2, name: "JavaScript" },
+// ];
+
+// courses.sort(function (a, b) {
+//   //these numbers represent the ascii table where the initials for the names are compared
+//   // a < b => -1
+//   // a > b => 1
+//   // a = b => 0
+//   const nameA = a.name.toUpperCase();
+//   const nameB = b.name.toUpperCase();
+
+//   //   if (a.name < b.name) return -1;
+//   //   if (a.name > b.name) return 1;
+//   if (nameA < nameB) return -1;
+//   if (nameA > nameB) return 1;
+//   return 0;
+// });
+// console.log(courses);

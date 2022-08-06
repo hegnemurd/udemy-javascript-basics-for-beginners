3eb2508924876b03178f7ec3a88b07a1b7726a19
hegnemurd <hegne.murd@gmail.com>
2022-08-06 21:00:09 +0100

    Section 6 - Combining and Slicing Arrays

--- section-6/part-one/part-one.js
+++ section-6/part-one/part-one.js
@@ -102,3 +102,33 @@
 
 // console.log(numbers);
 // console.log(another);
+
+// ----- COMBINING AND SLICING ARRAYS -----
+
+// // --- primitive values wil lhave primitive values are copied over
+
+// const first = [1, 2, 3];
+// const second = [4, 5, 6];
+
+// const combined = first.concat(second);
+
+// // const slice = combined.slice(2, 4);
+// // const slice = combined.slice(2);
+// const slice = combined.slice();
+
+// console.log(combined);
+// console.log(slice);
+
+// // --- with objects the references are copied and not the object -
+// // they will still reference to the same original object
+
+// const first = [{ id: 1 }];
+// const second = [4, 5, 6];
+
+// const combined = first.concat(second);
+// first[0].id = 10;
+
+// const slice = combined.slice();
+
+// console.log(combined);
+// console.log(slice);

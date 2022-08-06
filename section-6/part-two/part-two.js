3dce70de0e9adf0415814a94e07fd1cb64f63c90
hegnemurd <hegne.murd@gmail.com>
2022-08-06 21:22:46 +0100

    Section 6 - Joining Arrays

--- section-6/part-two/part-two.js
+++ section-6/part-two/part-two.js
@@ -9,3 +9,16 @@
 // // });
 
 // numbers.forEach((number, index) => console.log(index, number));
+
+// ----- JOINING ARRAYS -----
+
+// const numbers = [1, 2, 3];
+// const joined = numbers.join(",");
+// console.log(joined);
+
+// const message = "This is my first message";
+// const parts = message.split(" ");
+// console.log(parts);
+
+// const combined = parts.join("-");
+// console.log(combined);

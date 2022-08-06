70d5a689037e9b0c2a2b33a30a55faf8989ecc4f
hegnemurd <hegne.murd@gmail.com>
2022-08-06 21:46:20 +0100

    Section 6 - Filtering an Array

--- section-6/part-two/part-two.js
+++ section-6/part-two/part-two.js
@@ -68,3 +68,15 @@
 // });
 
 // console.log(atLeastOnePositive);
+
+// ----- FILTERING AN ARRAY -----
+
+// const numbers = [1, -1, 2, 3];
+
+// // const filtered = numbers.filter(function (value) {
+// //   return value >= 0;
+// // });
+
+// const filtered = numbers.filter((n) => n >= 0);
+
+// console.log(filtered);

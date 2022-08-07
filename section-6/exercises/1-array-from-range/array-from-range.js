3192871ffc72cb3cbfe83bbe9da232f32e746193
hegnemurd <hegne.murd@gmail.com>
2022-08-07 18:32:30 +0100

    Section 6 - Exercise 1

--- section-6/exercises/1-array-from-range/array-from-range.js
+++ section-6/exercises/1-array-from-range/array-from-range.js
@@ -1,4 +1,26 @@
 // ----- MY SOLUTION-----
+const numbers = arrayFromRange(-10, 4);
 
+console.log(numbers);
+
+function arrayFromRange(min, max) {
+  let newArray = [];
+  for (let i = min; i <= max; i++) {
+    newArray.push(i);
+  }
+  return newArray;
+}
 
 // ----- THEIR SOLUTION-----
+
+// const numbers = arrayFromRange(-10, -4);
+
+// console.log(numbers);
+
+// function arrayFromRange(min, max) {
+//   const output = [];
+//   for (let i = min; i <= max; i++) {
+//     output.push(i);
+//   }
+//   return output;
+// }

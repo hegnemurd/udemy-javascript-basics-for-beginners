db2b60192a39224e252ec6a0cce55707df4bd735
hegnemurd <hegne.murd@gmail.com>
2022-08-07 22:11:28 +0100

    Section 6 - Exercise 3

---
+++ section-6/exercises/3-except/except.js
@@ -0,0 +1,18 @@
+// ----- MY SOLUTION-----
+
+const numbers = [1, 2, 3, 4];
+
+const output = except(numbers, [1]);
+
+console.log(output);
+
+function except(array, excluded) {
+  for (let i = 0; i <= array.length; i++) {
+    if (array === excluded) {
+      array.splice(array[i], 1);
+    }
+  }
+  return array;
+}
+
+// ----- THEIR SOLUTION-----

6bbd0b9f3cfdb855a2a4f5a78a598c51e304a86d
hegnemurd <hegne.murd@gmail.com>
2022-08-12 21:46:19 +0100

    Section 6 - Exercise 4

--- section-6/exercises/4-moving-an-element/moving-an-element.js
+++ section-6/exercises/4-moving-an-element/moving-an-element.js
@@ -1,21 +1,56 @@
 // ----- MY SOLUTION-----
 
-const numbers = [1, 2, 3, 4];
+// const numbers = [1, 2, 3, 4];
 
-const output = move(numbers, 0, 1);
-// move 1 will move 1 over, offset dictates how much it moves over, 2 means it moves over 2 places.
-//offset can be negative: index = 1, offset = -1, moves 2 over to the front
+// const output = move(numbers, 1, -1);
+// // index 0 indicates which number to move, in this case number 1
+// // offset dictates how much it moves over, 1 means it moves over 1 place towards the back
+// //offset can be negative: index = 1, offset = -1, moves 2 over towards the front
 
-function move(array, index, offset) {
-  for (let element of array) {
-    element[index] = array[offset];
-  }
-  return array;
-}
+// function move(array, index, offset) {
+//   // for (let element of array) {
+//   //   element[index] = array[offset];
+//   // }
+//   // return array;
+//   let arrayLength = array.length;
+//   while (index < 0) {
+//     index += arrayLength;
+//   }
+//   while (offset < 0) {
+//     offset += arrayLength;
+//   }
+//   if (offset >= arrayLength) {
+//     var newIndex = offset - arrayLength + 1;
+//     while (newIndex--) {
+//       array.push(undefined);
+//     }
+//   } else if (index > arrayLength) {
+//     console.error("Invalid");
+//   }
+//   array.splice(offset, 0, array.splice(index, 1)[0]);
+//   return array;
+// }
 
-console.log(ouput);
+// console.log(output);
 
-console.error("Invalid");
 // create this method, when number is over the length then this error occurs
 
 // ----- THEIR SOLUTION-----
+
+const numbers = [1, 2, 3, 4];
+
+const output = move(numbers, 1, 0);
+
+console.log(output);
+
+function move(array, index, offset) {
+  const position = index + offset;
+  if (position >= array.length || position < 0) {
+    console.error("Invalid offset");
+    return;
+  }
+  const output = [...array];
+  const element = output.splice(index, 1)[0];
+  output.splice(position, 0, element);
+  return output;
+}

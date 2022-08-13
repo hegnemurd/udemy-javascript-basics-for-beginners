f0c7760c2cf30bcd46a6a2271d5aff6288c4867d
hegnemurd <hegne.murd@gmail.com>
2022-08-13 16:40:20 +0100

    Section 6 - Exercise 5

--- section-6/exercises/5-count-occurrences/count-occurrences.js
+++ section-6/exercises/5-count-occurrences/count-occurrences.js
@@ -4,9 +4,9 @@
 // if number does not exist in the array, return 0
 const numbers = [1, 1, 3, 2, 4, 3, 1, 11, 4];
 
-// const count = countOccurrences(numbers, -11);
+const count = countOccurrences(numbers, -11);
 
-const count = numbers.reduce(function countOccurrences(array, searchElement) {
+function countOccurrences(array, searchElement) {
   let occurrences = 0;
   for (let i = 0; i <= array.length; i++) {
     if (array[i] === searchElement) {
@@ -14,28 +14,37 @@ const count = numbers.reduce(function countOccurrences(array, searchElement) {
     }
   }
   return occurrences;
-});
+}
 
-console.log(count);
-
-// function countOccurrences(array, searchElement) {
+// const count = numbers.reduce(function countOccurrences(array, searchElement) {
 //   let occurrences = 0;
 //   for (let i = 0; i <= array.length; i++) {
 //     if (array[i] === searchElement) {
-//       occurrences++;
+//       occurrences + 1;
 //     }
 //   }
 //   return occurrences;
-// }
+// });
+
+console.log(count);
 
 // ----- THEIR SOLUTION-----
 
-// const numbers = [1, 2, 3, 4];
+// const numbers = [1, 1, 2, 3, 4];
 
 // const count = countOccurrences(numbers, 1);
 
 // console.log(count);
 
 // function countOccurrences(array, searchElement) {
+//   let count = 0;
+//   for (let element of array) if (element === searchElement) count++;
+//   return count;
+// }
 
+// function countOccurrences(array, searchElement) {
+//   return array.reduce((accumulator, current) => {
+//     const occurrence = current === searchElement ? 1 : 0;
+//     return accumulator + occurrence;
+//   }, 0);
 // }

023678655744e86d4defcbbf255e63115e3b5648
hegnemurd <hegne.murd@gmail.com>
2022-08-14 21:29:03 +0100

    Section 6 - Exercise 7

--- section-6/exercises/7-movies/movies.js
+++ section-6/exercises/7-movies/movies.js
@@ -12,8 +12,28 @@ const movies = [
 // Descending order
 // Pick their title
 
-movies
-  .filter((movie) => movie.rating > 4)
-  .map((movie) => console.log(movie.title + ": " + movie.rating));
+const newMovies = movies
+  .filter((movie) => movie.year === 2018 && movie.rating > 4)
+  .sort((a, b) => b.rating - a.rating)
+  .map((movie) => movie.title);
+
+console.log(newMovies);
 
 // ----- THEIR SOLUTION-----
+
+// const movies = [
+//   { title: "a", year: 2018, rating: 4.5 },
+//   { title: "b", year: 2018, rating: 4.7 },
+//   { title: "c", year: 2018, rating: 3 },
+//   { title: "d", year: 2017, rating: 4.5 },
+// ];
+
+// using letters can be better practice with readability
+
+// const titles = movies
+//   .filter((m) => m.year === 2018 && m.rating >= 4)
+//   .sort((a, b) => a.rating - b.rating)
+//   .reverse()
+//   .map((m) => m.title);
+
+// console.log(titles);

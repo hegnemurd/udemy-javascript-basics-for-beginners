e82107e6f3fa6500de993a68a103f60d6d3d5600
hegnemurd <hegne.murd@gmail.com>
2022-08-22 20:46:55 +0100

    Section 7 - Part Two

--- section-7/part-two/part-two.js
+++ section-7/part-two/part-two.js
@@ -21,3 +21,37 @@
 // }
 
 // console.log(person);
+
+// ----- LOCAL VS GLOBAL SCOPE-----
+
+// function start() {
+//   const message = "Hi"; // local scope and unreachable from outside
+
+//   if (true) {
+//     const another = "bye"; // local scope and unreachable from outside
+//   }
+
+// //   console.log(another); // undefined
+
+//   for (let i = 0; i<5;i++) {
+//     console.log(i); // 0 => 5
+//   }
+//   console.log(i); // undefined
+// }
+
+// console.log(message); // undefined
+
+// const color = "red"; // global constant
+
+// function start() {
+//   const message = "hi"; // local constant
+//   const color = "blue"; // local constant takes priority over global constant
+//   console.log(`${message} ${color}`);
+// }
+
+// function stop() {
+//   const message = "bye"; // local constant
+//   console.log(`${message} ${color}`);
+// }
+
+// start();

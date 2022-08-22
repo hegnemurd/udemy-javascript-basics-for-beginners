6d9e73df27a31dd3aa5ebdac58291a4badb03114
hegnemurd <hegne.murd@gmail.com>
2022-08-22 19:39:06 +0100

    Section 7 - Part One

--- section-7/part-one/part-one.js
+++ section-7/part-one/part-one.js
@@ -43,10 +43,32 @@
 
 // ----- ARGUMENTS -----
 
-function sum() {
-  let total = 0;
-  for (let value of arguments) total += value;
-  return total;
-}
+// function sum() {
+//   let total = 0;
+//   for (let value of arguments) total += value;
+//   return total;
+// }
 
-console.log(sum(1, 2, 3, 4, 5, 10));
+// console.log(sum(1, 2, 3, 4, 5, 10));
+
+// ----- THE REST OPERATOR -----
+
+// function sum(args) {
+//   console.log(args); // console shows only the first value
+// }
+
+// function sum(...args) {
+//   console.log(args); // console shows all values
+// }
+
+// function sum(...args) {
+//   return args.reduce((a, b) => a + b);
+// }
+
+// function sum(discount, ...prices) {
+//   // rest parameter has to be the last parameter, to be able to iterate all the rest of the parameters
+//   const total = prices.reduce((a, b) => a + b);
+//   return total * (1 - discount);
+// }
+
+// console.log(sum(0.1, 20, 30));

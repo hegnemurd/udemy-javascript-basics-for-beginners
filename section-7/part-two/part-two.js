31159e9d844a3836d3939424910775eabbba6787
hegnemurd <hegne.murd@gmail.com>
2022-08-22 20:33:29 +0100

    Section 7 - Part Two

---
+++ section-7/part-two/part-two.js
@@ -0,0 +1,23 @@
+// ----- TRY AND CATCH -----
+
+// const person = {
+//   firstName: "Jane",
+//   lastName: "Doe",
+//   set fullName(value) {
+//     if (typeof value !== "string") throw new Error("Value is not a string.");
+
+//     const parts = value.split(" ");
+//     if (parts.length !== 2) throw new Error("Enter a first and last name");
+//     this.firstName = parts[0];
+//     this.lastName = parts[1];
+//   },
+// };
+
+// // person.fullName = "John Doe";
+// try {
+//   person.fullName = "";
+// } catch (e) {
+//   alert(e);
+// }
+
+// console.log(person);

aa11f0856bfe2095f0d69285c0f2870e2f4ad207
hegnemurd <hegne.murd@gmail.com>
2022-08-22 20:08:30 +0100

    Section 7 - Part One

--- section-7/part-one/part-one.js
+++ section-7/part-one/part-one.js
@@ -97,4 +97,24 @@
 // // console.log(interest(10000, undefined, 5));
 // console.log(interest(10000, 5));
 
+// ----- GETTERS AND SETTERS -----
 
+// const person = {
+//   firstName: "Jane",
+//   lastName: "Doe",
+//   fullName() {
+//     return `${person.firstName} ${person.lastName}`;
+//   },
+//   set fullName(value) {
+//     const parts = value.split(" ");
+//     this.firstName = parts[0];
+//     this.lastName = parts[1];
+//   },
+// };
+
+// person.fullName = "John Doe";
+// // getters => access properties
+// // setters => change (mutate) them
+
+// // console.log(person.fullName());
+// console.log(person);

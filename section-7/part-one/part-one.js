1ee5a50cf669e2a01b10320a8845d6cb254b293e
hegnemurd <hegne.murd@gmail.com>
2022-08-22 18:30:12 +0100

    Section 7 - Part One

--- section-7/part-one/part-one.js
+++ section-7/part-one/part-one.js
@@ -42,3 +42,11 @@
 // };
 
 // ----- ARGUMENTS -----
+
+function sum() {
+  let total = 0;
+  for (let value of arguments) total += value;
+  return total;
+}
+
+console.log(sum(1, 2, 3, 4, 5, 10));

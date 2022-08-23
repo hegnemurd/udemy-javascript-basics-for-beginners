ebbffe797c24353d6b98655fa1f478df17cdc50b
hegnemurd <hegne.murd@gmail.com>
2022-08-23 20:43:10 +0100

    Section 7 - Part Two

--- section-7/part-two/part-two.js
+++ section-7/part-two/part-two.js
@@ -55,3 +55,50 @@
 // }
 
 // start();
+
+// ----- LET VS VAR -----
+
+// ES6 (ES2015): let, const => block-scoped
+// var => function-scoped
+
+// function start() {
+//   for (let i = 0; i < 5; i++) {
+//     console.log(i);
+//   }
+//   console.log(i); // cannot access i outside the for loop block
+// }
+
+// function start() {
+//   for (var i = 0; i < 5; i++) {
+//     console.log(i);
+//   }
+//   console.log(i); // can now access i even if outside the for loop
+// }
+
+// function start() {
+//   for (var i = 0; i < 5; i++) {
+//     if (true) {
+//       var color = "red";
+//     }
+//   }
+//   console.log(color);
+// }
+
+// function start() {
+//     for (var i = 0; i < 5; i++) {
+//       if (true) {
+//         let color = "red";
+//       }
+//     }
+//     console.log(i); // console shows 5
+//     console.log(color); // console shows error since it cannot access this variable
+//   }
+
+// start();
+
+// var color = "red"; // is added to the Window object, window.color => "red"
+// let age = 30; // is only available in the local scope
+
+// function sayHi() {
+//   console.log("Hi");
+// } // functions are added to the window object as well, window.sayHi() => "Hi" OR window.sayHi => sayHi function

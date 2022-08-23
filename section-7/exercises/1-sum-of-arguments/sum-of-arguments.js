06f2fede46e94d2a7924bfc8c320da9ffd235b7b
hegnemurd <hegne.murd@gmail.com>
2022-08-23 21:40:03 +0100

    Section 7 - Exercise 1

---
+++ section-7/exercises/1-sum-of-arguments/sum-of-arguments.js
@@ -0,0 +1,20 @@
+// ----- MY SOLUTION -----
+
+// sum(1, 2, 3, 4) => 10
+// sum([1, 2, 3, 4]) => 10
+
+function sum(numbers) {
+//   let total = 0;
+//   for (let i = 0; i <= numbers.length; i++) {
+//     total = i + i;
+//   }
+
+numbers.reduce();
+//   console.log(total);
+}
+
+sum(1, 2, 3, 4);
+
+// Array.isArray(); // if is => true otherwise => false
+
+// ----- THEIR SOLUTION -----

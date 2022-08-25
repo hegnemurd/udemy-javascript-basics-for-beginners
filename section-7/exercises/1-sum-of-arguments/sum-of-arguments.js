3b5c0c2d11c7cb5b1aab11025a347770164ac975
hegnemurd <hegne.murd@gmail.com>
2022-08-25 21:35:01 +0100

    Section 7 - Exercise 1

--- section-7/exercises/1-sum-of-arguments/sum-of-arguments.js
+++ section-7/exercises/1-sum-of-arguments/sum-of-arguments.js
@@ -3,35 +3,37 @@
 // sum(1, 2, 3, 4) => 10
 // sum([1, 2, 3, 4]) => 10
 
-function sum(numbers) {
-  //   let total = 0;
-  //   for (let i = 0; i <= numbers.length; i++) {
-  //     total = i + i;
-  //   }
+// function sum(numbers) {
+//   if (Array.isArray(numbers)) {
+//     const initialValue = 0;
+//     const sumValue = numbers.reduce(
+//       (previousValue, currentValue) => previousValue + currentValue,
+//       initialValue
+//     );
+//     console.log(sumValue);
+//     console.log(true);
+//   } else {
+//     let arrayNumbers = Array.prototype.reduce.call(
+//       numbers,
+//       function (previousValue, currentValue) {
+//         return previousValue + currentValue;
+//       },
+//       0
+//     );
+//     console.log(arrayNumbers);
+//     console.log(false);
+//   }
+// }
 
-  if (Array.isArray(numbers)) {
-    console.log(true);
-    const initialValue = 0;
-    const sumValue = numbers.reduce(
-      (previousValue, currentValue) => previousValue + currentValue,
-      initialValue
-    );
-    console.log(sumValue);
-  } else {
-    let sum = 0;
-    for (let i = 0; i <= numbers.length; i++) {
-      sum += i;
-    }
-    console.log(sum);
-    console.log(false);
-  }
-
-  //   console.log(total);
-}
-
-sum(1, 2, 3, 4);
-// sum([1, 2, 3, 4]);
-
-// Array.isArray(); // if is => true otherwise => false
+// sum(1, 2, 3, 4);
 
 // ----- THEIR SOLUTION -----
+
+console.log(sum([1, 2, 3, 4]));
+console.log(sum(1, 2, 3, 4));
+
+function sum(...items) {
+  if (items.length === 1 && Array.isArray(items[0])) items = [...items[0]];
+
+  return items.reduce((a, b) => a + b);
+}
